2d6ca62ac4df57afbc87088da35b524dd2eea06d
Vicente Calderon <thunderwearsvicho37@protonmail.com>
2020-12-23 13:46:10 -0300

    I'm too sleepy

---
+++ Components/Disk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapSim.Components {
+	/// <summary>
+	/// Represents the disk
+	/// </summary>
+	public sealed class Disk {
+
+	}
+}
